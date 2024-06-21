fa1f3013adb44e84c0fdfda81688702a2205976d
Imalsha Fernando <139493584+Nethu326Imal@users.noreply.github.com>
2024-06-21 22:48:04 +0530

    Upload select_year.php

---
+++ System/Teacher/select_year.php
@@ -0,0 +1,52 @@
+<?php
+
+    require_once('../../config.php');
+    session_start();
+    
+    $teacherId= $_SESSION["user"]["id"];
+
+    $_SESSION['year'] = '';
+    
+
+    if(!empty($_POST['selectedYear'])) {
+        $_SESSION['year'] = $_POST['selectedYear'];
+
+        if(!empty($_SESSION['course'])) {
+        
+            $courseCategory = $_SESSION['course'];
+            $courseYear = $_SESSION['year'];
+
+            $check = $courseCategory.$courseYear;
+    
+            $sql = "SELECT * FROM lectureAssign
+                    JOIN course ON
+                    course.courseId = lectureAssign.courseId
+                    JOIN teacher ON
+                    teacher.id = lectureAssign.teacherId
+                    WHERE 
+                    lectureAssign.courseId LIKE '$check%'
+                    AND
+                    teacherId = '$teacherId'";
+            
+            $result = $conn->query($sql);
+    
+            if($result->num_rows > 0) {
+                echo "
+                    <option value=''>Filtered Courses</option>";
+    
+                while($row = $result->fetch_assoc()) {
+                    $courseId = $row['courseId'];
+                    $courseName = $row['courseName'];
+                    echo"
+                        <option value='$courseId'>$courseId - $courseName</option>";
+                }
+            }
+            else {
+                echo"
+                    <option value=''>No Filtered Courses</option>";
+            }
+        }
+    }
+    
+    
+?>
