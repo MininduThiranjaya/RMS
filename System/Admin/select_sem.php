a7a261ba14052329c8b62ff8377de302c055b22c
SandeepaDilakshana <138925290+SandeepaDilakshana@users.noreply.github.com>
2024-06-22 14:16:30 +0530

    Add files via upload

---
+++ System/Admin/select_sem.php
@@ -0,0 +1,40 @@
+<?php
+    require_once('../../config.php');
+    session_start();
+
+    $_SESSION['sem'] = '';
+
+    if(!empty($_POST['selectedSem'])) {
+        $_SESSION['sem'] = $_POST['selectedSem'];
+    }
+
+    if(!empty($_SESSION['course']) && !empty($_SESSION['year']) && !empty($_SESSION['sem'])) {
+        
+        $courseCategory = $_SESSION['course'];
+        $courseYear = $_SESSION['year'];
+        $courseSem = $_SESSION['sem'];
+
+        $check = $courseCategory.$courseYear.$courseSem;
+
+        $sql = "SELECT * FROM course
+                WHERE courseId LIKE '$check%'";
+        
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0) {
+            echo "
+                <option value=''>Filtered Courses</option>";
+
+            while($row = $result->fetch_assoc()) {
+                $courseId = $row['courseId'];
+                $courseName = $row['courseName'];
+                echo"
+                    <option value='$courseId'>$courseId - $courseName</option>";
+            }
+        }
+        else {
+            echo"
+                <option value=''>No Filtered Courses</option>";
+        }
+    }
+?>
